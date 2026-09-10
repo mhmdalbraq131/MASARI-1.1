9e524c4c5f7f975258c7e2aea5faa169feaec808
mhmdalbraq131 <mhmdalbraq131@gmail.com>
2026-09-10 05:24:15 +0300

    fix: import ValueListenable for stable router

--- lib/app/app_router.dart
+++ lib/app/app_router.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 import 'package:go_router/go_router.dart';
 import '../core/security/protected_route_guard.dart';
 import '../features/admin/presentation/views/admin_operations_view.dart';
